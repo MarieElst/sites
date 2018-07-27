22b9e6e365a72f8f648ef057253912211672aa71
MarieElst <marie.elslt@gmail.com>
2018-07-27 16:44:46 +0200

    un message

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First document online</title>
+</head>
+<body>
+	<article>
+		<header>
+			<h1>Article Github</h1>
+			<p>Salut salut</p>
+		</header>
+		<main>
+			<section>
+				Du texte super cool 
+			</section>
+		</main>
+		<footer>
+			<p>Closing phrase.</p>
+		</footer>
+	</article>
+</body>
+</html>
